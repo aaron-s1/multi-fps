134b3aaf32283c92e565928eafcb514eed6aa353
aaron <aaron_swan@outlook.com>
2023-06-13 20:25:44 -0700

    cleaned up dash weapon script

--- Assets/Scripts/Combat/Player/Weapons/Dash_Weapon.cs
+++ Assets/Scripts/Combat/Player/Weapons/Dash_Weapon.cs
@@ -5,83 +5,40 @@ using UnityEngine;
 
 public class Dash_Weapon : MonoBehaviour, IFireable
 {
-    
-    [SerializeField] GameObject player;
-    [SerializeField] Rigidbody playerRigid;
+
+    [SerializeField] float levitateDuration;
+    [SerializeField] float levitateDistance;
+
+    [SerializeField] float dashDuration;
+    [SerializeField] float dashDistance;
+
+    GameObject player;
+    Rigidbody playerRigid;
+      
+    float elapsedTime = 0f;
+
     bool acceptPlayerInputs;
-
-    [Space(10)]
-    [SerializeField] float dashSpeed = 2f;
-    // [SerializeField] float dashDuration = 2f;
-    
-
-    FirstPersonLook playerCamera;
-    float originalCameraSensitivity;
-
-    bool canFire;
-    bool isDashing;
-
-    [Space(10)]
-    [SerializeField] float tiltCameraFrequency = 0.5f; 
-    [SerializeField] float tiltCameraDuration = 5f;
-    [SerializeField] float tiltCameraRange = 1f;
-    float tiltTimeElapsed = 0f;
-    bool isTiltingForward = true;
 
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).gameObject;
         playerRigid = player.GetComponent<Rigidbody>();
-
-        playerCamera = player.GetComponentInChildren<FirstPersonLook>();
-        originalCameraSensitivity = playerCamera.sensitivity;
-
-
-
-        
     }
 
 
-    public void Fire(GameObject instance)
-    {
-        StartCoroutine(StartLevitation2(player.transform));
-        // StartCoroutine(StartDash(player.transform));
-
-    }
-
-    void FixedUpdate()
-    {
-        
-    }
-
-    // public Transform startMarker; // Starting position
-    // public Transform endMarker; // Ending position
-    float elapsedTime = 0f; // Elapsed time for the ler
-
-    [Space(40)]
-    Vector3 startLevitatePosition;
-    Vector3 endLevitatePosition;
-
-    public float levitateDuration;
-    public float levitateDistance;
-
-    Vector3 endDashPosition;
-
-    public float dashDuration;
-    public float dashDistance;
+    public void Fire(GameObject instance) =>
+        StartCoroutine(StartLevitation(player.transform));
 
 
-
-    IEnumerator StartLevitation2(Transform playerTransform)
+    IEnumerator StartLevitation(Transform playerTransform)
     {
         acceptPlayerInputs = player.GetComponent<FirstPersonMovement>().acceptingMovementInput = false;
 
         elapsedTime = 0f;
-        startLevitatePosition = new Vector3(player.transform.localPosition.x, player.transform.localPosition.y, player.transform.localPosition.z);
-        endLevitatePosition = new Vector3(startLevitatePosition.x, startLevitatePosition.y + levitateDistance, startLevitatePosition.z);
+        Vector3 startLevitatePosition = new Vector3(player.transform.localPosition.x, player.transform.localPosition.y, player.transform.localPosition.z);
+        Vector3 endLevitatePosition = new Vector3(startLevitatePosition.x, startLevitatePosition.y + levitateDistance, startLevitatePosition.z);
 
-        // endDashPosition = new Vector3(player.transform.localPosition.x, player.transform.localPosition.y, player.transform.localPosition.z + dashDistance);
-        endDashPosition = player.transform.localPosition + playerTransform.forward * dashDistance;
+        Vector3 endDashPosition = player.transform.localPosition + playerTransform.forward * dashDistance;
 
 
         while (elapsedTime < levitateDuration)
@@ -93,14 +50,12 @@ public class Dash_Weapon : MonoBehaviour, IFireable
 
         player.transform.localPosition = endLevitatePosition;
 
-        yield return StartCoroutine(StartDash2(playerTransform));        
+        yield return StartCoroutine(StartDash(playerTransform, endLevitatePosition, endDashPosition));        
     }
 
-    IEnumerator StartDash2(Transform playerTransform)
+    IEnumerator StartDash(Transform playerTransform, Vector3 endLevitatePosition, Vector3 endDashPosition)
     {
         elapsedTime = 0f;
-
-        // endLevitatePosition = GetLerpTarget(playerTransform, levitateDistance);
 
         while (elapsedTime < dashDuration)
         {
@@ -113,111 +68,47 @@ public class Dash_Weapon : MonoBehaviour, IFireable
 
         player.transform.localPosition = endDashPosition;
         acceptPlayerInputs = player.GetComponent<FirstPersonMovement>().acceptingMovementInput = false;
-        Debug.Log("dash has ended");
-        Debug.Log(player.transform.localPosition);
 
         yield break;
     }
 
 
+    // Tilt camera variables.
+    // [SerializeField] float tiltCameraFrequency = 0.5f; 
+    // [SerializeField] float tiltCameraDuration = 5f;
+    // [SerializeField] float tiltCameraRange = 1f;
+    // FirstPersonLook playerCamera;
+    // float originalCameraSensitivity;
+    // float tiltTimeElapsed = 0f;
+    // bool isTiltingForward = true;
 
-    // Vector3 GetLerpTarget(Transform playerTransform, Vector3 distanceOffset) 
-    // {
-    //     return new Vector3 (
-    //     player.transform.localPositionx + distanceOffset.x,
-    //     player.transform.localPositiony + distanceOffset.y,
-    //     player.transform.localPositionz + distanceOffset.z
-    //     );
-    // }
-
-
-    IEnumerator StartLevitation(Transform playerTransform)
-    {
-        acceptPlayerInputs = player.GetComponent<FirstPersonMovement>().acceptingMovementInput = false;
-        // Invoke("EndLevitation", levitationTime);
-        isLevitating = true;
-        // playerRigid.useGravity = false;
-
-        // This makes the  while loop not work.
-        // playerRigid.isKinematic = true;
-
-        while (isLevitating)
-        {
-            playerRigid.velocity = playerTransform.up * 3f;
-            yield return null;
-        }
-
-        if (!isLevitating & !isDashing)
-            StartCoroutine(StartDash(playerTransform));
-
-        yield return null;
-    }
-
-    void EndLevitation()
-    {
-        // playerRigid.useGravity = true;
-        playerRigid.isKinematic = false;
-        isLevitating = false;
-    }
-
+            // playerCamera = player.GetComponentInChildren<FirstPersonLook>();
+            // originalCameraSensitivity = playerCamera.sensitivity;
     
-    bool isLevitating;
-    // public float levitationTime;
+//    void TiltCamera()
+//     {
+//         playerCamera.sensitivity = 0;
 
+//         if (!isDashing)
+//             return;        
 
-    IEnumerator StartDash(Transform playerTransform)
-    {
-        Invoke("EndDash", dashDuration);
-        isDashing = true;
-               
-        // acceptPlayerInputs = player.GetComponent<FirstPersonMovement>().acceptingMovementInput = false;
-        
-        // add look at + latch onto enemy.
-        
-        while (isDashing)
-        {            
-            TiltCamera();
-            playerRigid.velocity = playerTransform.forward * dashSpeed;
-            playerRigid.velocity = -playerTransform.up * dashSpeed * 0.2f;
-            yield return null;
-        }
+//         tiltTimeElapsed += Time.deltaTime;
 
-        player.GetComponent<FirstPersonMovement>().acceptingMovementInput = true;
+//         if (tiltTimeElapsed >= tiltCameraFrequency)
+//         {
+//             tiltTimeElapsed = 0f;
+//             isTiltingForward = !isTiltingForward;            
+//         }
 
-        transform.rotation = Quaternion.identity;
-    }
+//         float tiltAmount = Mathf.PingPong(tiltTimeElapsed / tiltCameraFrequency, 1f);
+//         if (!isTiltingForward)
+//             tiltAmount = 1f - tiltAmount;
 
-    void EndDash() 
-    {
-        playerCamera.sensitivity = originalCameraSensitivity;
-        isDashing = false;
-    }
+//         float tiltZ = Mathf.Lerp(-tiltCameraRange, tiltCameraRange, Mathf.Abs(tiltAmount - 0.5f) * 2f);
 
-    
-   void TiltCamera()
-    {
-        playerCamera.sensitivity = 0;
+//         Vector3 rotation = playerCamera.transform.rotation.eulerAngles;
 
-        if (!isDashing)
-            return;        
-
-        tiltTimeElapsed += Time.deltaTime;
-
-        if (tiltTimeElapsed >= tiltCameraFrequency)
-        {
-            tiltTimeElapsed = 0f;
-            isTiltingForward = !isTiltingForward;            
-        }
-
-        float tiltAmount = Mathf.PingPong(tiltTimeElapsed / tiltCameraFrequency, 1f);
-        if (!isTiltingForward)
-            tiltAmount = 1f - tiltAmount;
-
-        float tiltZ = Mathf.Lerp(-tiltCameraRange, tiltCameraRange, Mathf.Abs(tiltAmount - 0.5f) * 2f);
-
-        Vector3 rotation = playerCamera.transform.rotation.eulerAngles;
-
-        rotation.z = tiltZ;
-        playerCamera.transform.rotation = Quaternion.Euler(rotation);
-    }
+//         rotation.z = tiltZ;
+//         playerCamera.transform.rotation = Quaternion.Euler(rotation);
+//     }
 }
